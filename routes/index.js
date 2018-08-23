f8975a7578f015931f8de297bd622b6414ae87ad
Jason James <jasonajames@live.com>
2018-08-23 11:46:01 -0700

    removed meta data from views and added metadata to router

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,10 @@ router.get('/fontana', function(req, res, next) {
     service5: 'Construction CleanUp',
     location1: 'Eastvale',
     location2: 'Fontana',
-    location3: 'San Dimas'
+    location3: 'San Dimas',
+    keywords: 'Window Cleaning, Window Screen Repair, Pressure Washing, Construction CleanUp, Christmas Light Installation, at your service, home remodeling, home repair, window screen install, window screen service, at your service, ays, fontana',
+    metadesc: 'At Your Service (AYS) Fontana offers Window cleaning, window screen repair, power washing, construction cleanup, and Christmas light installation services to Fontana and Surrounding Areas.'
+
    });
 });
 
@@ -47,11 +50,14 @@ router.get('/san-dimas', function(req, res, next) {
     service5: 'Construction CleanUp',
     location1: 'Eastvale',
     location2: 'Fontana',
-    location3: 'San Dimas'
+    location3: 'San Dimas',
+    keywords: 'Window Cleaning, Window Screen Repair, Pressure Washing, Construction CleanUp, Christmas Light Installation, at your service, home remodeling, home repair, window screen install, window screen service, at your service, ays, san dimas',
+    metadesc: 'At Your Service (AYS) San Dimas offers Window cleaning, window screen repair, power washing, construction cleanup, and Christmas light installation services to San Dimas and Surrounding Areas.'
+
    });
 });
 
-/* GET Mira Loma page. */
+/* GET Eastvale page. */
 router.get('/eastvale', function(req, res, next) {
   res.render('index', { title: 'At Your Service', city: 'Eastvale', pagetitle: 'AtYourService1USA.com Eastvale',
     imagelocation: '/images/AYS-Eastvale.png',
@@ -63,7 +69,10 @@ router.get('/eastvale', function(req, res, next) {
     service5: 'Construction CleanUp',
     location1: 'Eastvale',
     location2: 'Fontana',
-    location3: 'San Dimas'
+    location3: 'San Dimas',
+    keywords: 'Window Cleaning, Window Screen Repair, Pressure Washing, Construction CleanUp, Christmas Light Installation, at your service, home remodeling, home repair, window screen install, window screen service, at your service, ays, eastvale',
+    metadesc: 'At Your Service (AYS) Eastvale offers Window cleaning, window screen repair, power washing, construction cleanup, and Christmas light installation services to Eastvale and Surrounding Areas.'
+
    });
 });
 
